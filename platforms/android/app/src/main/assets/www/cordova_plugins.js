84ff0a002bdbe7f9dc459c199304eafd2ee47db2
Kaspars Jaudzems <kasparsj@gmail.com>
2023-04-11 00:26:24 +0300

    fix android call from native

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,10 +7,27 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "cpp"
       ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification",
+      "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification_android",
+      "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-cpp-bridge": "0.0.7",
+    "cordova-plugin-dialogs": "3.0.0-dev",
     "cordova-plugin-homework": "1.0.0",
     "cordova-plugin-add-swift-support": "2.0.2"
   };
